83f83dc95fbdb6f802ccfaa6832f2ef801f2de01
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-07-19 13:39:23 +0800

    done with chat system but without the image processing

--- server/dynamodb/models.py
+++ server/dynamodb/models.py
@@ -1,14 +1,15 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 class message(BaseModel):
+    room_id: str
     message_id: str
-    order_id: str
     sender_id: str
     receiver_id: str
     content: str
     created_at: str
     updated_at: str
-    read_status: str
+    read_status: bool = False
 
 class notification(BaseModel):
     notifiaction_id: str
@@ -24,14 +25,33 @@ class review(BaseModel):
     reviewer_id: str
     reviewee_id: str
     reviewer_type: str
-    product_id: str | None
-    order_id: str | None
+    product_id: str | None = None
+    order_id: str | None = None
     rating: float
     description: str
-    images: list[str]
-    created_at: str
-    updated_at: str
+    images: list[str] | None = None
+    created_at: datetime
+    updated_at: datetime
     reported: bool = False
+
+class raw_review(BaseModel):
+    review_id: str
+    reviewer_id: str
+    reviewee_id: str
+    reviewer_type: str
+    product_id: str | None = None
+    order_id: str | None = None
+    rating: float
+    description: str
+    images: list[str] | None = None
+    reported: bool = False
+
+class update_review(BaseModel):
+    rating: float | None = None
+    description: str | None = None
+    images: list[str] | None = None
+    reported: bool | None = None
+
 
 class report(BaseModel):
     report_id: str
