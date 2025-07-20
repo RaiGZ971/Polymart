432e755de61510ad006d1a965e5c492c1d9c3c3c
Julianna Leila Castillo <juliannaleilacastillo@gmail.com>
2025-07-20 20:14:55 +0800

    feat: notifications implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import { LandingPage, SignUp, SignIn } from './pages'
 import NotificationTest from "./pages/general-pages/NotificationTest";
 import LandingPage from './pages/general-pages/LandingPage'
 import SignUp from './pages/general-pages/SignUp'
