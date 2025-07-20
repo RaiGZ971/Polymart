90b110db2325e0be56edf6823add206cc7a99ed0
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-07-20 21:32:30 +0800

    minor additional route in main file

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from supabase.routes import router as supabase_router
+# from supabase.routes import router as supabase_router
 from dynamodb.routes import router as dynamodb_router
 from auth.routes import router as auth_router
+from s3.routes import router as s3_router
 
 app = FastAPI()
 
@@ -19,6 +20,7 @@ app.add_middleware(
 
 )
 
-app.include_router(supabase_router, prefix="/supabase", tags=["Supabase"])
+# app.include_router(supabase_router, prefix="/supabase", tags=["Supabase"])
 app.include_router(dynamodb_router, prefix="/dynamodb", tags=["DyanamoDB"])
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(s3_router, prefix="/s3", tags=["S3 Bucket"])
