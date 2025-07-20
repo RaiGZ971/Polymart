d7a06c8c2dde6a175c8ba2d1c6661e06bab81003
FvlSky <mariahannavb@gmail.com>
2025-07-20 16:14:09 +0800

    Feat: Notification Overlay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import { LandingPage, SignUp, SignIn } from './pages'
+import NotificationTest from "./pages/general-pages/NotificationTest";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/notification-test" element={<NotificationTest />} />
         </Routes>
       </div>
     </Router>
