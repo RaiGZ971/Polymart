d209e4bb3c5d1212bfae97e2a7e86cb747e2e4cf
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-07-20 19:04:10 +0800

    complete message, notification, review, and report endpoint but without image processing in s3

--- server/dynamodb/models.py
+++ server/dynamodb/models.py
@@ -8,19 +8,26 @@ class message(BaseModel):
     message_id: str          
     sender_id: str
     receiver_id: str
-    content: str
+    content: str | None = None
+    image: str | None = None
     created_at: str                     #sort-key
     updated_at: str
     read_status: bool = False
 
 class notification(BaseModel):
-    notifiaction_id: str
     user_id: str                        #partition-key
+    notification_id: str
     notification_type: str
     content: str
     related_id: str
     seen: bool = False
     timestamp: str                      #sort-key
+
+class raw_notification(BaseModel):
+    user_id: str
+    notification_type: str
+    content: str
+    related_id: str
 
 class review(BaseModel):
     reviewee_id: str                    #partition-key
@@ -37,7 +44,6 @@ class review(BaseModel):
     reported: bool = False
 
 class raw_review(BaseModel):
-    review_id: str
     reviewer_id: str
     reviewee_id: str
     reviewer_type: str
@@ -62,8 +68,16 @@ class report(BaseModel):
     reported_entity_id: str
     reason: str
     description: str
-    status: str
+    status: str | None = "Not Checked"
     created_at:str                      #sort-key
+
+class raw_report(BaseModel):
+    reporter_id: str
+    reporter_entity_type: str
+    reported_entity_id: str
+    reason: str
+    description: str
+
 
 
 
--- server/dynamodb/utils.py
+++ server/dynamodb/utils.py
@@ -8,17 +8,6 @@ def get_room(sender_id, receiver_id) -> str:
 def get_current_date() -> str:
     return str(datetime.now(UTC))
 
-def process_review_form(form) -> models.review:
-    currentDate = get_current_date()
-
-    updatedForm = {
-        **form.model_dump(),
-        "created_at": currentDate,
-        "updated_at": currentDate
-    }
-
-    return models.review(**updatedForm)
-
 def process_message_form( room_id: str, message: str, sender_id: str, receiver_id: str) -> models.message:
     messageID = str(uuid.uuid4())
     currentDate = get_current_date()
@@ -35,3 +24,40 @@ def process_message_form( room_id: str, message: str, sender_id: str, receiver_i
     }
 
     return models.message(**formattedResponse)
+
+def process_review_form(form: models.raw_review) -> models.review:
+    reviewID = str(uuid.uuid4())
+    currentDate = get_current_date()
+
+    updatedForm = {
+        **form.model_dump(),
+        "review_id": reviewID,
+        "created_at": currentDate,
+        "updated_at": currentDate
+    }
+
+    return models.review(**updatedForm)
+
+def process_report_form(form: models.raw_report) -> models.report:
+    reportID = str(uuid.uuid4())
+    currentDate = get_current_date()
+
+    updatedForm = {
+        **form.model_dump(),
+        "report_id": reportID,
+        "created_at": currentDate,
+    }
+
+    return models.report(**updatedForm)
+
+def process_notification_form(form: models.raw_notification) -> models.notification:
+    notificationID = str(uuid.uuid4())
+    currentDate = get_current_date()
+
+    updatedForm = {
+        **form.model_dump(),
+        "notification_id": notificationID,
+        "timestamp": currentDate
+    }
+    
+    return models.notification(**updatedForm)
