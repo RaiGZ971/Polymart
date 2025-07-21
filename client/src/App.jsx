9ff0dad8eec2c12aa75bd3ca6163756951e7e908
FvlSky <mariahannavb@gmail.com>
2025-07-21 21:08:52 +0800

    feat: Leave a review component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/general-pages/SignUp'
 import SignIn from './pages/general-pages/SignIn'
 import GeneralDashboard from './pages/general-pages/GeneralDashboard'
 import TestPage from './pages/general-pages/TestPage'
+import LeaveReviewPreview from './pages/general-pages/LeaveReviewPreview'; //Remove once leave review component is finalized
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import OrdersMeetups from './pages/general-pages/OrdersMeetups'
 import ViewProductDetails from './pages/buyer-pages/ViewProductDetails';
@@ -25,6 +26,7 @@ function App() {
           <Route path="/dashboard" element={<GeneralDashboard />} />
           <Route path="/orders-meetups" element={<OrdersMeetups />} />
           <Route path="/testpage" element={<TestPage />} />
+          <Route path="/leave-review-preview" element={<LeaveReviewPreview />} />
           <Route path="/buyer/view-product-details" element={<ViewProductDetails />} />
           {/* Add more routes as needed */}
         </Routes>
--- client/src/pages/general-pages/index.js
+++ client/src/pages/general-pages/index.js
@@ -5,3 +5,5 @@ export { default as SignUp } from './SignUp';
 export { default as SignIn } from './SignIn';
 export { default as GeneralDashboard } from './GeneralDashboard';
 export { default as TestPage } from './TestPage';
+
+export { default as LeaveReviewPreview } from './LeaveReviewPreview';
