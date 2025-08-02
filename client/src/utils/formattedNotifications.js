314bf3487a1719e0fe982fa33b64f5e7b79704b3
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-08-03 05:13:35 +0800

    added the tanstack query for easy handling api calls supporting catching, and more. Uses the zustand properly

--- client/src/utils/formattedNotifications.js
+++ client/src/utils/formattedNotifications.js
@@ -1,24 +1,24 @@
-import { TimeAgo } from "./timeAgo.js";
+import { TimeAgo } from './timeAgo.js';
 
-export const formattedNotifications = async (notifications) => {
-    let responses = []
-    notifications.map((notification) => {
-        let title = "";
-        let time = TimeAgo(notification.timestamp)
+export const formattedNotifications = (notifications) => {
+  let responses = [];
+  notifications.map((notification) => {
+    let title = '';
+    let time = TimeAgo(notification.timestamp);
 
-        if(notification.notification_type === "meetup"){
-            title = "Meetup Reminder";
-        }else if(notification.notification_type === "listing-approved"){
-            title = "Listing Approved";
-        }
+    if (notification.notification_type === 'meetup') {
+      title = 'Meetup Reminder';
+    } else if (notification.notification_type === 'listing-approved') {
+      title = 'Listing Approved';
+    }
 
-        responses.push({
-            "type": notification.notification_type,
-            "title": title,
-            "time": time,
-            "message": notification.content
-        })
-    })
+    responses.push({
+      type: notification.notification_type,
+      title: title,
+      time: time,
+      message: notification.content,
+    });
+  });
 
-    return responses;
-}
+  return responses;
+};
