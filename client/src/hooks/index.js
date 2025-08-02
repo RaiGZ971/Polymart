125383b22dd4f601401cc61c26fc78e9641ab692
Reymel Sardenia <sardeniareymel@gmail.com>
2025-08-03 07:43:04 +0800

    feat: implement dashboard and orders data hooks, add services for listings and orders

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,3 +1,5 @@
 export { useSignUpNavigation } from "./useSignUpNavigation";
 export { useEmailVerification } from "./useEmailVerification";
 export { default as useOrderModals } from "./useOrderModals";
+export { useOrdersData } from "./useOrdersData";
+export { useDashboardData } from "./useDashboardData";
--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,3 +1,6 @@
 export { ChatService } from './chatService.js';
 export { ApiClient } from './apiClient.js';
 export { AuthService } from './authService.js';
+export { ListingService } from './listingService.js';
+export { OrderService } from './orderService.js';
+export { UserService } from './userService.js';
