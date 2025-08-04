7c5244ef2efe45e75bcc49a3d5c8add47177546f
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-08-04 15:14:29 +0800

    added notification

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,20 +1,11 @@
-import { Sidebar, SidebarOpen } from 'lucide-react';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const useAppStore = create(
+export const useAuthStore = create(
   persist((set, get) => ({
-    //UI State
-    theme: 'light',
-
-    //User State
     currentUser: null,
     isAuthenticated: false,
 
-    //Actions
-    setTheme: (theme) => set({ theme }),
-
-    //Auth actions
     setUser: (user) => set({ currentUser: user, isAuthenticated: true }),
     logout: () => ({
       currentUser: null,
@@ -23,9 +14,10 @@ export const useAppStore = create(
     }),
   })),
   {
-    name: 'polymart-app-state',
+    name: 'polymart-user-stete',
     partialize: (state) => ({
-      theme: state.theme,
+      currentUser: state.currentUser,
+      isAuthenticated: state.isAuthenticated,
     }),
   }
 );
