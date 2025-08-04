8451e5056b6d5df679e1cde06c1d2f372989de97
Reymel Sardenia <sardeniareymel@gmail.com>
2025-08-04 13:56:08 +0800

    feat:  improve data caching and loading states for fast lookups

--- client/src/components/layout/MainDashboard.jsx
+++ client/src/components/layout/MainDashboard.jsx
@@ -1,11 +1,14 @@
 import { NavigationDashboard, Footer } from "..";
 
-export default function MainDashboard({ children }) {
+export default function MainDashboard({ children, onLogoClick, onHomeClick }) {
   return (
     <>
       <div className="min-h-screen bg-white flex flex-col items-center mb-20">
         <div className="w-full px-0 mx-0">
-          <NavigationDashboard />
+          <NavigationDashboard 
+            onLogoClick={onLogoClick}
+            onHomeClick={onHomeClick}
+          />
         </div>
         <div className="w-full flex-1 flex flex-col items-center">
           {children}
