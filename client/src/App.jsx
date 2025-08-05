439785419942f352e4dfe4b41f685952bc5b1022
Reymel Sardenia <sardeniareymel@gmail.com>
2025-08-05 09:48:52 +0800

    feat: add new placeholder pages for Profile, Liked Items, Customer Service, Help, and Manage Listing; update navigation paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/general-pages/SignUp";
 import SignIn from "./pages/general-pages/SignIn";
 import GeneralDashboard from "./pages/general-pages/GeneralDashboard";
+import Profile from "./pages/general-pages/Profile";
+import LikedItems from "./pages/general-pages/LikedItems";
+import CustomerService from "./pages/general-pages/CustomerService";
+import Help from "./pages/general-pages/Help";
+import ManageListing from "./pages/general-pages/ManageListing";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import OrdersMeetups from "./pages/general-pages/OrdersMeetups";
 import ViewProductDetails from "./pages/buyer-pages/ViewProductDetails";
@@ -20,6 +25,11 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/dashboard" element={<GeneralDashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/liked-items" element={<LikedItems />} />
+          <Route path="/customer-service" element={<CustomerService />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/manage-listing" element={<ManageListing />} />
           <Route path="/orders-meetups" element={<OrdersMeetups />} />
           <Route
             path="/buyer/view-product-details/:id?"
---
+++ client/src/pages/general-pages/Help.jsx
@@ -0,0 +1,14 @@
+import { MainDashboard } from "../../components";
+
+export default function Help() {
+  return (
+    <MainDashboard>
+      <div className="w-[80%] mt-10 flex flex-col items-center justify-center min-h-[400px]">
+        <h1 className="text-4xl font-bold text-primary-red mb-8">Help</h1>
+        <div className="text-lg text-gray-500">
+          Help page coming soon...
+        </div>
+      </div>
+    </MainDashboard>
+  );
+}
